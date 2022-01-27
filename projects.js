5034f893a1e3198e6a36348455ea02865e48426b
Meredith Bloom <meredithbloom@merediths-imac.lan>
2022-01-27 16:12:55 -0500

    created projects html css and js files. copied base from resume files

---
+++ projects.js
@@ -0,0 +1,87 @@
+
+/////code for main dropdown menu
+const $menubtn = $('#modal-menu-btn')
+const $menu = $('.dropdown-menu')
+const $closeBtn = $('#close-menu-btn')
+
+const showMenu = () => {
+  $menu.css('display', 'flex');
+  $menubtn.css('display', 'none');
+  $closeBtn.css('display', 'inline-block');
+}
+
+const closeMenu = () => {
+  $menu.css('display', 'none');
+  $closeBtn.css('display', 'none')
+  $menubtn.css('display', 'inline-block')
+}
+
+
+///////// skills dropdown
+
+const $skillsList = $('.skills-list')
+const $skillsOpen = $('#skills-header')
+const $skillsClose = $('#close-skills')
+
+//
+// const showSkills = () => {
+//   $skillsList.css('display','block');
+//   $skillsClose.css('display', 'block');
+//   // $skillsOpen.css('display', 'none');
+// }
+//
+// const hideSkills = () => {
+//   $skillsList.css('display', 'none');
+//   $skillsClose.css('display', 'none');
+  // $skillsOpen.css('display', 'block')
+// }
+
+
+//////// achievements & awards dropdown
+
+
+
+///////site credits dropdown
+const $openCredits = $('#open-credits')
+const $credits = $('.icon-credits')
+const $closeCredits = $('#close-credits')
+
+
+const showCredits = () => {
+  $credits.css('display', 'block');
+  $openCredits.css('display','none');
+  $closeCredits.css('display', 'inline-block');
+}
+
+const closeCredits = () => {
+  $credits.css('display', 'none');
+  $closeCredits.css('display', 'none')
+  $openCredits.css('display', 'inline-block')
+}
+
+
+
+//////DOM load function
+$(() => {
+
+
+  ////top menu jquery
+  $menubtn.on('click', showMenu);
+  $closeBtn.on('click', closeMenu);
+
+  ////open skills dropdown
+  // $skillsOpen.on('click', showSkills);
+  // $skillsClose.on('click', hideSkills)
+
+
+
+
+  ///// credit footer jquery
+  $openCredits.on('click', showCredits);
+  $closeCredits.on('click', closeCredits);
+
+
+
+
+
+});
