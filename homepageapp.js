d5ce276909f9f59222f524acb7ac12d68e541e76
Meredith Bloom <meredithbloom@merediths-imac.lan>
2022-01-26 22:46:00 -0500

    got the dropdown menu to work

---
+++ homepageapp.js
@@ -0,0 +1,28 @@
+
+
+
+
+const $menubtn = $('#modal-menu-btn')
+const $menu = $('.dropdown-menu')
+const $closeBtn = $('#close-menu-btn')
+
+const showMenu = () => {
+  $menu.css('display', 'flex');
+  $menubtn.css('display', 'none');
+  $closeBtn.css('display', 'inline-block');
+}
+
+const closeMenu = () => {
+  $menu.css('display', 'none');
+  $closeBtn.css('display', 'none')
+  $menubtn.css('display', 'inline-block')
+}
+
+
+
+$(() => {
+
+  $menubtn.on('click', showMenu);
+  $closeBtn.on('click', closeMenu);
+
+});
