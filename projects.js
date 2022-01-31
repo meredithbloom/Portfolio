e2d37a7f2f53cff9a9a60d35f5610a4c64fc2495
Meredith Bloom <meredithbloom@merediths-imac.lan>
2022-01-31 18:53:25 -0500

    improved desktop and laptop projects page

--- projects.js
+++ projects.js
@@ -34,7 +34,6 @@ const closeModal = () => {
 
 
 
-
 ///////site credits dropdown
 const $openCredits = $('#open-credits')
 const $credits = $('.icon-credits')
@@ -55,15 +54,9 @@ const closeCredits = () => {
 
 
 
-////carousel images for project page
-
-const carouselCYOA = ['carouselpics/us1.jpeg', 'carouselpics/thetallgrass.jpeg', 'carouselpics/cube.jpeg', 'carouselpics/theshining.jpeg', 'carouselpics/itfollows.png']
-const carouselProjects = ['carouselpics/flowerbox1.png']
-const carouselPics = ['carouselpics/cowboybebop.jpeg', 'carouselpics/envy-pointing.jpeg', 'carouselpics/howl2.jpeg', 'carouselpics/Howls-Moving-Castle.jpeg', 'carouselpics/howlsmovingcastle1.jpg']
-
-
-
 /////image carousel PROJECT 1
+const carouselCYOA = ['carouselpics/us1.jpeg', 'carouselpics/thetallgrass.jpeg', 'carouselpics/cube.jpeg', 'carouselpics/theshining.jpeg', 'carouselpics/itfollows.png']
+
 const $nextBtn1 = $('.forward#firstproject');
 const $backBtn1 = $('.back#firstproject');
 const $currentImg1 = $('.carousel-pic#1');
@@ -96,6 +89,71 @@ const prevImg1 = () => {
 
 
 /////image carousel PROJECT 2
+const carouselProjects = ['carouselpics/flowerbox1.png', 'carouselpics/crazycharlie2.jpeg']
+
+const $nextBtn2 = $('.forward#secondproject');
+const $backBtn2 = $('.back#secondproject');
+const $currentImg2 = $('.carousel-pic#2');
+
+let numOfImgs2 = carouselProjects.length;
+let maxIndex2 = numOfImgs2-1;
+let currentImgIndex2 = 0;
+
+const nextImg2 = () => {
+  if (currentImgIndex2 < maxIndex2) {
+    currentImgIndex2++
+    $currentImg2.attr('src', carouselProjects[currentImgIndex2]);
+  } else if (currentImgIndex2 == maxIndex2) {
+    currentImgIndex2 = 0;
+    $currentImg2.attr('src', carouselProjects[currentImgIndex2])
+  }
+}
+
+const prevImg2 = () => {
+  if (currentImgIndex2 > 0) {
+    currentImgIndex2--
+    $currentImg2.attr('src', carouselProjects[currentImgIndex2]);
+  } else if (currentImgIndex2 == 0) {
+    currentImgIndex2 = maxIndex2;
+    $currentImg2.attr('src', carouselProjects[currentImgIndex2]);
+  }
+}
+
+
+///image carousel PROJECT 3
+const carouselPics = ['carouselpics/cowboybebop.jpeg', 'carouselpics/envy-pointing.jpeg', 'carouselpics/howl2.jpeg', 'carouselpics/Howls-Moving-Castle.jpeg', 'carouselpics/howlsmovingcastle1.jpg']
+
+
+const $nextBtn3 = $('.forward#thirdproject');
+const $backBtn3 = $('.back#thirdproject');
+const $currentImg3 = $('.carousel-pic#3');
+
+let numOfImgs3 = carouselPics.length;
+let maxIndex3 = numOfImgs3-1;
+let currentImgIndex3 = 0;
+
+
+const nextImg3 = () => {
+  if (currentImgIndex3 < maxIndex3) {
+    currentImgIndex3++
+    $currentImg3.attr('src', carouselPics[currentImgIndex3]);
+  } else if (currentImgIndex3 == maxIndex3) {
+    currentImgIndex3 = 0;
+    $currentImg3.attr('src', carouselPics[currentImgIndex3])
+  }
+}
+
+const prevImg3 = () => {
+  if (currentImgIndex3 > 0) {
+    currentImgIndex3--
+    $currentImg3.attr('src', carouselPics[currentImgIndex3]);
+  } else if (currentImgIndex3 == 0) {
+    currentImgIndex3 = maxIndex3;
+    $currentImg3.attr('src', carouselPics[currentImgIndex3]);
+  }
+}
+
+
 
 
 //////DOM load function
@@ -113,13 +171,17 @@ $(() => {
   $openCredits.on('click', showCredits);
   $closeCredits.on('click', closeCredits);
 
-  ////image carousel
+  ////first project image carousel
   $nextBtn1.on('click', nextImg1);
   $backBtn1.on('click', prevImg1);
 
+  ///second project image carousel
+  $nextBtn2.on('click', nextImg2);
+  $backBtn2.on('click', prevImg2);
 
-
-
+  //third project image carousel
+  $nextBtn3.on('click', nextImg3);
+  $backBtn3.on('click', prevImg3);
 
 
 
